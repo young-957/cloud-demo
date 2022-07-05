3efecba69177bee1a5507ea3d797dc3c2562beea
Young <1806198497@qq.com>
2022-07-05 08:27:17 +0800

    Initial commit

---
+++ eureka-server/src/main/java/com/rhd/eureka/EurekaApplication.java
@@ -0,0 +1,20 @@
+package com.rhd.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author 任恒德
+ * @version 1.0
+ * @date 2022/6/20 22:05
+ * @description
+ */
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+}
---
+++ user-service/src/main/java/cn/itcast/user/web/UserController.java
@@ -0,0 +1,49 @@
+package cn.itcast.user.web;
+
+import cn.itcast.user.pojo.User;
+import cn.itcast.user.service.UserService;
+import com.alibaba.nacos.api.config.annotation.NacosValue;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Slf4j
+@RestController
+@RequestMapping("/user")
+@RefreshScope
+public class UserController {
+
+//    @NacosValue("${pattern.dataformat}")
+//    private String dateformat;
+
+    @Value("${pattern.dataformat}")
+    private String dateformat;
+
+    @GetMapping("now")
+    public String now(){
+        if(dateformat!=null){
+            System.out.println(dateformat);
+        }else System.out.println("GG");
+
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern(dateformat));
+    }
+
+    @Autowired
+    private UserService userService;
+
+    /**
+     * 路径： /user/110
+     *
+     * @param id 用户id
+     * @return 用户
+     */
+    @GetMapping("/{id}")
+    public User queryById(@PathVariable("id") Long id) {
+        return userService.queryById(id);
+    }
+}
